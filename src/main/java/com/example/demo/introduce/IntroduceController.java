ef483d747731854e4e0352d22d1baeb35cd09207
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-12 20:18:53 +0900

    style: 줄바꿈 정리

--- src/main/java/com/example/demo/introduce/IntroduceController.java
+++ src/main/java/com/example/demo/introduce/IntroduceController.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IntroduceController {
-
-
-
+    
     @GetMapping("/introduce")
     @ResponseBody
     public String getNameIntroduce(@RequestParam(required = false) String name) {
---
+++ src/main/java/com/example/demo/json/JsonController.java
@@ -0,0 +1,4 @@
+package com.example.demo.json;
+
+public class JsonController {
+}
