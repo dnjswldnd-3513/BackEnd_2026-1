a43d28abe8d6854ce78556f2006f44b2c8e4c2db
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-12 20:20:19 +0900

    feat: json 관련 클래스 및 기능 구현

--- src/main/java/com/example/demo/json/Entity.java
+++ src/main/java/com/example/demo/json/Entity.java
@@ -1,4 +1,20 @@
 package com.example.demo.json;
 
 public class Entity {
+
+    private String name;
+    private Integer age;
+
+    public Entity(Integer age, String name) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age.toString();
+    }
 }
--- src/main/java/com/example/demo/json/JsonController.java
+++ src/main/java/com/example/demo/json/JsonController.java
@@ -1,4 +1,16 @@
 package com.example.demo.json;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class JsonController {
+
+
+    @GetMapping("/json")
+    @ResponseBody
+    public Entity get(){
+        return  new Entity(26,"허준기");
+    }
 }
