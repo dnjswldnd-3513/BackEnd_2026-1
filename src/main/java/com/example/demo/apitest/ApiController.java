813d7a23584480dc9a8c038701fe00cae58183ba
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-12 20:29:55 +0900

    feat: ApiController Get메서드 기능 추가

--- src/main/java/com/example/demo/apitest/ApiController.java
+++ src/main/java/com/example/demo/apitest/ApiController.java
@@ -1,4 +1,17 @@
-package com.example.demo.apiTest;
+package com.example.demo.apitest;
 
-public class Controller {
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ApiController {
+
+
+    @GetMapping("/api/{index}")
+    public ResponseEntity<Long> getMethod(@PathVariable Long index){
+        return ResponseEntity.ok(index);
+    }
 }
