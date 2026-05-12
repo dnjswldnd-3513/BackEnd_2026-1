d27f9f8a9d6e121bbe2e56e05c122cb09e18e6eb
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-12 21:52:43 +0900

    refactor: JsonController class, Controller --> RestController

--- src/main/java/com/example/demo/json/JsonController.java
+++ src/main/java/com/example/demo/json/JsonController.java
@@ -3,13 +3,13 @@ package com.example.demo.json;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class JsonController {
 
 
     @GetMapping("/json")
-    @ResponseBody
     public Entity get(){
         return new Entity(26L,"허준기");
     }
