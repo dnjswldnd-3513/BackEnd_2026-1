2acbf46ce59dafedfca2a557ccc5cd2bbaf90eda
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-12 20:53:13 +0900

    feat: 생성관련 기능 추가

--- src/main/java/com/example/demo/apitest/ApiController.java
+++ src/main/java/com/example/demo/apitest/ApiController.java
@@ -1,17 +1,30 @@
 package com.example.demo.apitest;
 
 
+import com.example.demo.json.Entity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class ApiController {
 
+    Map<Long,Aricle> maps = new HashMap<>();
 
     @GetMapping("/api/{index}")
-    public ResponseEntity<Long> getMethod(@PathVariable Long index){
-        return ResponseEntity.ok(index);
+    @ResponseBody
+    public ResponseEntity<?> getMethod(@PathVariable Long index){
+        return ResponseEntity.ok(maps.get(index));
+    }
+
+    @PostMapping("/api")
+    @ResponseBody
+    public ResponseEntity<?> postMethod(@RequestBody Aricle aricle){
+        maps.put(aricle.getId(),aricle);
+        return ResponseEntity.status(HttpStatus.CREATED).body(aricle);
     }
 }
