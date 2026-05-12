610fd253e732c3451dd01190b85ba3731298b3e3
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-12 20:07:06 +0900

    feat: RequestParam 형태로 요청했을때 텍스트가 반환되도록 하기 기능 생성

--- src/main/java/com/example/demo/introduce/IntroduceController.java
+++ src/main/java/com/example/demo/introduce/IntroduceController.java
@@ -2,13 +2,20 @@ package com.example.demo.introduce;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IntroduceController {
 
+    
+
     @GetMapping("/introduce")
-    public String introduce(){
-        return "introduce";
+    @ResponseBody
+    public String getNameIntroduce(@RequestParam(required = false) String name) {
+        return name;
     }
 
 }
+
