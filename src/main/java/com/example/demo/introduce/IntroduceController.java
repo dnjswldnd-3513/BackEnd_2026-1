25101be1b154ae3cb3c84fb9573fe45d46b28c19
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-12 21:52:02 +0900

    refactor: IntroduceController class, Controller --> RestController

--- src/main/java/com/example/demo/introduce/IntroduceController.java
+++ src/main/java/com/example/demo/introduce/IntroduceController.java
@@ -1,16 +1,12 @@
 package com.example.demo.introduce;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class IntroduceController {
 
     @GetMapping("/introduce")
-    @ResponseBody
     public String getNameIntroduce(@RequestParam(required = false) String name) {
         if (name == null){
             return "안녕하세요. 저는 원지웅입니다.";
