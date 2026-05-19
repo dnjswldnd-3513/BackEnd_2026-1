c42c382b2ef3b1ff968f3b3c0dee878e92b575ad
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-19 19:54:27 +0900

    fix: 세번째 및 네번째 피드백 introduce html 반환과 출력형식

--- src/main/java/com/example/demo/introduce/IntroduceController.java
+++ src/main/java/com/example/demo/introduce/IntroduceController.java
@@ -1,17 +1,21 @@
 package com.example.demo.introduce;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class IntroduceController {
 
     @GetMapping("/introduce")
-    public String getNameIntroduce(@RequestParam(required = false) String name) {
-        if (name == null){
-            return "안녕하세요. 저는 원지웅입니다.";
-        }
-        return name;
+    public String introduce() {
+        return "introduce";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/introduce", params = "name")
+    public String getNameIntroduce(@RequestParam String name) {
+        return "안녕하세요 제 이름은 "+name+"입니다!";
     }
 
 }
