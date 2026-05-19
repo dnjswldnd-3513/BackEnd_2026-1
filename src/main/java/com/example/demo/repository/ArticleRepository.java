dac740a2a26a998befa43c4d3070178f4ad95b18
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-19 20:35:11 +0900

    feat: ArticleRepository기능 추가

--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -4,6 +4,8 @@ import com.example.demo.entity.Article;
 import com.example.demo.entity.Board;
 import com.example.demo.entity.Member;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,46 +15,49 @@ public class ArticleRepository {
     private Map<Long, Article> articles = new HashMap<>();
     private Map<Long, Member> members = new HashMap<>();
     private Map<Long, Board> boards = new HashMap<>();
-
-    public ArticleRepository() {
-
-    }
-    public void initMembers(){
-
-    }
-    public void initBoards(){
-
-    }
-    public void initArticles(){
-
-    }
+    private Long articleNextId = 0L;
 
     public List<Article> findAll(){
-        return  null;
+        return  new ArrayList<>(articles.values());
     }
 
     public Article findById(Long id){
-        return  null;
+        Article article = articles.get(id);
+        if (article == null) throw new IllegalArgumentException("존재하지 않는 article id: " + id);
+        return article;
     }
 
     public Member findMemberById(Long id){
-        return null;
+        Member member = members.get(id);
+        if (member == null) throw new IllegalArgumentException("존재하지 않는 member id: " + id);
+        return member;
     }
 
     public Board findBoardById(Long id){
-        return null;
+        Board board = boards.get(id);
+        if (board == null) throw new IllegalArgumentException("board id: " + id);
+        return board;
     }
 
-    public Article save(){
-        return null;
+    public Article save(Article article){
+        article.setId(articleNextId);
+        article.setCreatedAt(LocalDateTime.now());
+        article.setUpdatedAt(LocalDateTime.now());
+        articles.put(article.getId(),article);
+        return article;
     }
 
-    public Article update(){
-        return null;
+    public Article update(Long id,String title,String content){
+        Article article = findById(id);
+        article.setTitle(title);
+        article.setContent(content);
+        article.setUpdatedAt(LocalDateTime.now());
+        return article;
     }
 
-    public void delete(){
-
+    public void delete(Long id){
+        if (!articles.containsKey(id)) throw new IllegalArgumentException("존재하지 않는 article id:" +id);
+        articles.remove(id);
     }
 
 }
