e37762993014168a4e4a5b65f61df68fb4ac4431
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-19 20:56:38 +0900

    feat: 6주차를 위한 PostController class 생성

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -1,4 +1,23 @@
 package com.example.demo.controller;
 
+
+import com.example.demo.repository.ArticleRepository;
+import com.example.demo.service.ArticleService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PostController {
+    private ArticleService articleService;
+
+    public PostController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping("/posts")
+    public String getPosts(Model model){
+        articleService.setPostsModel(model);
+        return "posts";
+    }
 }
