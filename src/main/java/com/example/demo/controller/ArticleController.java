6f9d3bdc04e4c2830a5fb5d825fcb50d3f0113bb
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-19 20:52:58 +0900

    feat: 6주차를 위한 Article class 구조 바꾸기

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -2,50 +2,50 @@ package com.example.demo.controller;
 
 
 import com.example.demo.entity.Article;
+import com.example.demo.service.ArticleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class ArticleController {
 
-    Map<Long, Article> maps = new HashMap<>();
+    private ArticleService articleService;
 
-    @GetMapping("/article/{index}")
-    public ResponseEntity<?> getMethod(@PathVariable Long index){
-        if (maps.get(index) == null){
-            return ResponseEntity.notFound().build(); // notFound,notContent의 따라 에러코드도 달라짐
-        }
-        return ResponseEntity.ok(maps.get(index));
+    @GetMapping("/article")
+    public ResponseEntity<List<Map<String, Object>>> getArticles(){
+        return ResponseEntity.ok(articleService.getArticles());
+    }
+
+    @GetMapping("/article/{id}")
+    public ResponseEntity<Map<String, Object>> getArticle(@PathVariable Long id){
+        return ResponseEntity.ok(articleService.getArticle(id));
     }
 
     @PostMapping("/article")
-    public ResponseEntity<?> postMethod(@RequestBody Article aricle){
-        maps.put(aricle.getId(),aricle);
-        return ResponseEntity.status(HttpStatus.CREATED).body(aricle);
+    public ResponseEntity<Article> createArticle(@RequestBody Map<String,Object> body){
+        Long memberId = Long.valueOf(body.get("memberId").toString());
+        Long boardId = Long.valueOf(body.get("boardId").toString());
+        String title = body.get("title").toString();
+        String content = body.get("content").toString();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(articleService.createArticle(memberId, boardId, title, content));
     }
 
     @PutMapping("/article/{id}")
-    public ResponseEntity<?> putMethod(@PathVariable Long id, @RequestBody Article aricle) {
-        if (maps.get(id) == null) {
-            return ResponseEntity.notFound().build();
-        }
-        Article updates = maps.get(id);
-        updates.setDescription(aricle.getDescription());
-        maps.put(id, updates);
-        return ResponseEntity.ok(updates);
+    public ResponseEntity<Article> updateArticle(@PathVariable Long id, @RequestBody Map<String,Object> body) {
+        String title = body.get("title").toString();
+        String content = body.get("content").toString();
+        return ResponseEntity.ok(articleService.updateArticle(id,title,content));
     }
 
-
     @DeleteMapping("/article/{id}")
-    public ResponseEntity<?> deleteMethod(@PathVariable Long id){
-        if (maps.get(id) == null){
-            return ResponseEntity.notFound().build();
-        }
-        maps.remove(id) ;
+    public ResponseEntity<?> deleteArticle(@PathVariable Long id){
+        articleService.deleteArticle(id);
         return ResponseEntity.noContent().build();
     }
 }
