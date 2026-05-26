c86fb2b11ca4f72e53504885eed25c2a9dbaea87
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-26 21:18:08 +0900

    feat: BoardController class 생성

---
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Board;
+import com.example.demo.service.BoardService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class BoardController {
+
+    private final BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @GetMapping("/boards")
+    public ResponseEntity<List<Board>> getBoards() {
+        return ResponseEntity.ok(boardService.getBoards());
+    }
+
+    @GetMapping("/boards/{id}")
+    public ResponseEntity<Board> getBoard(@PathVariable Long id) {
+        return ResponseEntity.ok(boardService.getBoard(id));
+    }
+
+    @PostMapping("/boards")
+    public ResponseEntity<Board> create(@RequestBody Map<String, Object> body) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(boardService.createBoard(body.get("name").toString()));
+    }
+
+    @PutMapping("/boards/{id}")
+    public ResponseEntity<Board> update(@PathVariable Long id, @RequestBody Map<String, Object> body) {
+        return ResponseEntity.ok(boardService.updateBoard(id, body.get("name").toString()));
+    }
+
+    @DeleteMapping("/boards/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        boardService.deleteBoard(id);
+        return ResponseEntity.noContent().build();
+    }
+}
