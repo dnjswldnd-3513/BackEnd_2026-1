d57a29dd779b0cfa3a0e69928ba7fa17cc45eee3
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-26 21:07:47 +0900

    feat: MemberService class 생성

---
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,39 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Member;
+import com.example.demo.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberService {
+
+    public final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public List<Member> getMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member getMember(Long id){
+        return memberRepository.findById(id);
+    }
+
+    public Member createMember(String name, String email,String pass){
+        return memberRepository.save(new Member(name,email,pass));
+    }
+
+    public Member updateMember(Long id, String name, String email,String pass){
+        Member member = memberRepository.findById(id);
+        member.update(name,email,pass);
+        return member;
+    }
+
+    public void deleteMember(Long id){
+        memberRepository.deleteById(id);
+    }
+}
