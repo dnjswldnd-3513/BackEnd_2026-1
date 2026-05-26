1605cf0124939b649bcd35223b554c7b7a758372
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-26 20:32:42 +0900

    feat: 7주차를 위한 MemberRepository 생성

--- src/main/java/com/example/demo/repository/MemberReposiitory.java
+++ src/main/java/com/example/demo/repository/MemberReposiitory.java
@@ -1,4 +1,45 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
 public class MemberReposiitory {
+
+    private final Map<Long, Member> members = new HashMap<>();
+    private Long nextId = 0L;
+
+
+    public MemberReposiitory() {
+        //testcode
+        save(new Member("테스트유저이름","testemail","1234"));
+    }
+
+    public Member save(Member member){
+        member.createId(++nextId);
+        members.put(member.getId(),member);
+        return member;
+    }
+
+    public Member findbyId(Long id){
+        Member member = members.get(id);
+        if(member == null){
+            throw new IllegalArgumentException("존재하지 않는 member id " + id);
+        }
+        return member;
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(members.values());
+    }
+
+    public void deleteById(Long id) {
+        members.remove(id);
+    }
+    
 }
