187327e3c2538a6f369ce27447e50f7a815bf814
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-26 20:40:04 +0900

    feat: 7주차를 위한 BoardRepository 생성

--- src/main/java/com/example/demo/repository/BoardRepository.java
+++ src/main/java/com/example/demo/repository/BoardRepository.java
@@ -1,4 +1,36 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Board;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
 public class BoardRepository {
+
+    private final Map<Long, Board> boards = new HashMap<>();
+    private Long nextId = 0L;
+
+    public Board save(Board board){
+        board.crateId(++nextId);
+        boards.put(board.getId(),board);
+        return board;
+    }
+
+    public Board findById(Long id){
+        Board board = boards.get(id);
+        if (board == null) throw new IllegalArgumentException("존재하지 않는 board id: " + id);
+        return board;
+    }
+
+    public List<Board> findAll() {
+        return new ArrayList<>(boards.values());
+    }
+
+    public void deleteById(Long id) {
+        boards.remove(id);
+    }
 }
