3b88a6041630218f2322baa29345e23a1a541fcb
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-26 20:39:47 +0900

    docs: MemberRepository 오타 수정

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -9,13 +9,13 @@ import java.util.List;
 import java.util.Map;
 
 @Repository
-public class MemberReposiitory {
+public class MemberRepository {
 
     private final Map<Long, Member> members = new HashMap<>();
     private Long nextId = 0L;
 
 
-    public MemberReposiitory() {
+    public MemberRepository() {
         //testcode
         save(new Member("테스트유저이름","testemail","1234"));
     }
