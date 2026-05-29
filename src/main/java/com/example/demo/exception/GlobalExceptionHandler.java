d856c2cfc295ff66433ac41e9f20a9e52a4a8c24
dnjswldnd-3513 <zheld3513@gmail.com>
2026-05-29 14:36:55 +0900

    feat: GlobalExceptionHandler 추가

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,4 +1,30 @@
 package com.example.demo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map<String,String>> handleNotFound(EntityNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Map.of("error",e.getMessage()));
+    }
+
+    @ExceptionHandler(DuplicationEmailException.class)
+    public ResponseEntity<Map<String,String>> handleConflict(DuplicationEmailException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(Map.of("error",e.getMessage()));
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Map<String,String>> handleBadRequest(BadRequestException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body((Map.of("error",e.getMessage())));
+    }
 }
