a5a387f5ac6eb61fb56e0c07a1fe499081d8e775
dnjswldnd-3513 <zheld3513@gmail.com>
2026-06-22 13:40:57 +0900

    feat: JDBC 과제 - PostController 파라미터 추가 및 뷰 수정

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -1,23 +1,33 @@
 package com.example.demo.controller;
 
-
-import com.example.demo.repository.ArticleRepository;
-import com.example.demo.service.ArticleService;
+import com.example.demo.dao.ArticleDao;
+import com.example.demo.dao.BoardDao;
+import com.example.demo.entity.Article;
+import com.example.demo.entity.Board;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class PostController {
-    private ArticleService articleService;
 
-    public PostController(ArticleService articleService) {
-        this.articleService = articleService;
+    private final ArticleDao articleDao;
+    private final BoardDao boardDao;
+
+    public PostController(ArticleDao articleDao, BoardDao boardDao) {
+        this.articleDao = articleDao;
+        this.boardDao = boardDao;
     }
 
     @GetMapping("/posts")
-    public String getPosts(Model model){
-        articleService.setPostsModel(model);
+    public String getPosts(@RequestParam Long boardId, Model model) {
+        Board board = boardDao.findById(boardId);
+        List<Article> articles = articleDao.findByBoardId(boardId);
+        model.addAttribute("boardName", board.getName());
+        model.addAttribute("articles", articles);
         return "posts";
     }
 }
