192a715dafab4605c7abe913bfafb6b1aad8e16f
dnjswldnd-3513 <zheld3513@gmail.com>
2026-06-22 18:35:47 +0900

    fix: JPA 과제 - 줄간격 수정

--- src/main/java/com/example/demo/dao/MemberDao.java
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -21,12 +21,12 @@ public class MemberDao {
 
     public Member findById(Long id) {
         Member member = entityManager.find(Member.class, id);
-        if (member == null) throw new EntityNotFoundException("존재하지 않는 member id: "+ id);
+        if (member == null) throw new EntityNotFoundException("존재하지 않는 member id: " + id);
         return member;
     }
 
     public List<Member> findAll() {
-        return entityManager.createQuery("SELECT m FROM Member m",Member.class).getResultList();
+        return entityManager.createQuery("SELECT m FROM Member m", Member.class).getResultList();
     }
 
     public void deleteById(Long id) {
@@ -36,14 +36,14 @@ public class MemberDao {
 
     public boolean checkByEmail(String email) {
         Long count = entityManager.createQuery(
-                "SELECT COUNT(m) FROM Member m WHERE m.email = :email",Long.class)
+                        "SELECT COUNT(m) FROM Member m WHERE m.email = :email", Long.class)
                 .setParameter("email", email)
-                .setParameter("email",email)
+                .setParameter("email", email)
                 .getSingleResult();
         return count > 0;
     }
 
-    public boolean checkByEmailAndNot(Long id,String email) {
+    public boolean checkByEmailAndNot(Long id, String email) {
         Long count = entityManager.createQuery(
                         "SELECT COUNT(m) FROM Member m WHERE m.email = :email AND m.id != :id", Long.class)
                 .setParameter("email", email)
@@ -51,8 +51,9 @@ public class MemberDao {
                 .getSingleResult();
         return count > 0;
     }
+
     public boolean existsById(Long id) {
-        Long count = entityManager.createQuery("SELECT COUNT(m) FROM Member m WHERE m.id = :id",Long.class)
+        Long count = entityManager.createQuery("SELECT COUNT(m) FROM Member m WHERE m.id = :id", Long.class)
                 .setParameter("id", id)
                 .getSingleResult();
         return count > 0;
