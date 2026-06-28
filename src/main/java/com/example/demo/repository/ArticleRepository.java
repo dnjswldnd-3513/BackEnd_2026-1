49ba3fde737b73fdd7f9b3bb5c035ba90dcf3d2e
dnjswldnd-3513 <zheld3513@gmail.com>
2026-06-28 10:35:02 +0900

    fix: JPA 과제.2 - 자동완성으로 인한 Long --> String 객체 타입이라 오류 변경

--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article,Long> {
-    List<Article> findByBoardId(String email);
+    List<Article> findByBoardId(Long boardId);
 }
