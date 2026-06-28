4e81cb510e749cb6672cad562bd1a38173f5d1e2
dnjswldnd-3513 <zheld3513@gmail.com>
2026-06-28 10:03:06 +0900

    feat: JPA 과제.2 -> Entity 연관관계 매핑 사용 -> Board) OneToMany 사용

--- src/main/java/com/example/demo/entity/Board.java
+++ src/main/java/com/example/demo/entity/Board.java
@@ -2,15 +2,21 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "board")
 public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false, unique = true)
     private String name;
 
+    @OneToMany(mappedBy = "board",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Article> articles = new ArrayList<>();
     public Board() {
     }
 
@@ -32,5 +38,12 @@ public class Board {
 
     public String getName() {
         return name;
+    }
+
+    public void addArticles(Article article) {
+        articles.add(article);
+    }
+    public List<Article> getArticles() {
+        return articles;
     }
 }
