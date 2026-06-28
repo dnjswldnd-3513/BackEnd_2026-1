1502b1789defb1e6a650cd7cfe05f6388532abbc
dnjswldnd-3513 <zheld3513@gmail.com>
2026-06-28 10:09:16 +0900

    feat: JPA 과제.2 - Spring Data JPa repository 추가 및 EntityfManager Dao 삭제

---
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ArticleRepository extends JpaRepository<Article,Long> {
+    List<Article> findByBoardId(String email);
+}
