6ed9197ebe3bea8bce04344422d5efa85d4d2592
dnjswldnd-3513 <zheld3513@gmail.com>
2026-07-01 12:34:06 +0900

    week-10-로그인 api 추가, MemberController 수정

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.request.LoginRequest;
 import com.example.demo.dto.request.MemberCreateRequest;
 import com.example.demo.dto.request.MemberUpdateRequest;
+import com.example.demo.dto.response.LoginResponse;
 import com.example.demo.dto.response.MemberResponse;
 import com.example.demo.entity.Member;
 import com.example.demo.service.MemberService;
@@ -46,5 +48,10 @@ public class MemberController {
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         memberService.deleteMember(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/members/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(memberService.login(request));
     }
 }
